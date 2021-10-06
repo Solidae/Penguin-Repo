7e3b420d9666b8cdf6dc27594dd2e727abfaf185
Solidae <91126864+Solidae@users.noreply.github.com>
2021-10-06 16:05:29 +0100

    form name change

--- PenguinForm03/Form1.Designer.cs
+++ PenguinForm03/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace PenguinForm03
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(644, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Penguin Index";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
